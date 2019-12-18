cb361ea4141540f5254da597d8a2a18660a12ff8
rajsudeep <sudrocks@gmail.com>
2019-12-18 22:09:22 +0000

    Add skipping

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -31,12 +31,12 @@ class TestDBStorage(unittest.TestCase):
         self.cursor.close()
         self.db.close()
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'db')
     def test_pep8_DBStorage(self):
         """Tests pep8 style"""
         style = pep8.StyleGuide(quiet=True)
         p = style.check_files(['models/engine/file_storage.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
-
 
 if __name__ == "__main__":
     unittest.main()
