51fc0acb50750c8600816b2f53d4d0e2b68aa813
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-12-19 20:59:25 +0000

    Commit for task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """teardown_db"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """states_list"""
+    storage.reload()
+    state_list = []
+
+    for state in storage.all('State').values():
+        state_list.append([state.id, state.name])
+    return render_template('7-states_list.html', states=state_list)
+
+
+if __name__ == "__main__":
+    app.run()
