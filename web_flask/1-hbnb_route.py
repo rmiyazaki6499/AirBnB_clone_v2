1575cb75517dd12b6b23a58eff959f065c1aa3e8
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-12-19 19:19:32 +0000

    Commit for task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """hello_route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """hbnb_route"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
